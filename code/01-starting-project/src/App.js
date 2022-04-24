6fa75b2357220fbf536d3051243e8f6ee957531f
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-04-24 15:28:57 +0530

    8.1-> adding-user-component

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import AddUser from "./components/UI/Users/AddUser"
 
-function App() {
+const  App= () => {
   return (
     <div>
 
+      <AddUser />
+      
     </div>
   );
 }
