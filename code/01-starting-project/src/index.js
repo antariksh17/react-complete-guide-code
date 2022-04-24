adc2b144d71b9f75256d73615ceb8a3208be1e81
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-04-24 12:23:46 +0530

    7.2->understanding-react-error-messages

--- code/01-starting-project/src/index.js
+++ code/01-starting-project/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as  ReactDOMClient from 'react-dom/client';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = ReactDOMClient.createRoot(rootElement);
+
+root.render(
+
+    <App />
+
+);
