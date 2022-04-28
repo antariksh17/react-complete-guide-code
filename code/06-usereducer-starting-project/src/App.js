3e339e9f5ca119e6bd496d09516c74bdb1a4ef81
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-04-28 19:03:22 +0530

    10.16->making-context-dynamic

--- code/06-usereducer-starting-project/src/App.js
+++ code/06-usereducer-starting-project/src/App.js
@@ -33,10 +33,11 @@ function App() {
       
       value={{
 
-        isLoggedIn: isLoggedIn
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
       }}>
       
-        <MainHeader  onLogout={logoutHandler} />
+        <MainHeader />
         <main>
           {!isLoggedIn && <Login onLogin={loginHandler} />}
           {isLoggedIn && <Home onLogout={logoutHandler} />}
