aca0d46e97cec2d769a5d0575d02ae9b680302c6
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-02 18:43:06 +0530

    11.19->useEffect-hook-to-show-cart-bump

--- code/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ code/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css'
@@ -8,14 +8,36 @@ const HeaderCartButton = (props) => {
 
     const cartCtx= useContext(CartContext);
 
-    const numberOfCardItems = cartCtx.items.reduce((curNumber, item) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+    const {items} = cartCtx;
+     
+    const numberOfCardItems = items.reduce((curNumber, item) => {
 
         return curNumber + item.amount;
 
     },0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlighted? classes.bump: ''}`;
+
+
+    useEffect( () => {
+       if(items.length >0){
+            setBtnIsHighlighted(true); 
+       }
+        
+       const timer= setTimeout( () => {
+           setBtnIsHighlighted(false);
+       },300);
+
+       return () => {
+           clearTimeout(timer); 
+       }
+
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
 
                 <CartIcon />
