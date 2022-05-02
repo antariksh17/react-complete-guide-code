e4f52bf4aaf9d636972337b8c58973bbda6520a2
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-02 18:12:23 +0530

    11.18-> making-items-removable

--- code/01-starting-project/src/components/Cart/Cart.js
+++ code/01-starting-project/src/components/Cart/Cart.js
@@ -16,10 +16,15 @@ const Cart = (props) => {
 
     const hasItems = cartCtx.items.length > 0;
 
-    const cartItemRemoveHandler = id => {};
+    const cartItemRemoveHandler = id => {
+        cartCtx.removeItem(id);
+    };
 
 
-    const cartItemAddHandler = id => {};
+    const cartItemAddHandler = (item) => {
+
+        cartCtx.addItem({...item, amount: 1});
+    };
 
     const cartItems = (<ul className={classes['cart-items']}> {cartCtx.items.map(item => 
     <CartItem 
@@ -28,7 +33,7 @@ const Cart = (props) => {
             amount={item.amount} 
             price={item.price}
             onRemove={cartItemRemoveHandler.bind(null,item.id)}
-            onAdd={cartItemAddHandler.bind(null,item.id)}
+            onAdd={cartItemAddHandler.bind(null,item)}
         />)}
     </ul>);
 
