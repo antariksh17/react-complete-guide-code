d1c027386c3609436ed3288c2d3f38537168e20e
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-06 05:32:40 +0530

    17.5->handling-errors

--- code/01-starting-project/src/components/Meals/AvailableMeals.js
+++ code/01-starting-project/src/components/Meals/AvailableMeals.js
@@ -3,6 +3,95 @@ import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
+// cant throw error inside a promise because it will cause the promise to reject
+
+
+const AvailableMeals = () => {
+
+  const [meals, setMeals] =useState([]);
+  const [isLoading , setIsLoading] =useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect( () => {
+
+    const fetchMeals = async() => {
+
+      const response = await fetch('https://reactcg-http-default-rtdb.firebaseio.com/Meals.json')
+
+      if(!response.ok){
+        throw new Error("Failed to Fetch")
+
+      }
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for(const key in responseData){
+        loadedMeals.push({
+          id:key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        })
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false)
+
+    }
+    
+      fetchMeals().catch(error => {  // HACKS to error handle=> catching the error we are getting
+
+        setIsLoading(false);
+        setHttpError(error.message)
+      });
+    
+   
+
+  },[])
+
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  if(isLoading){
+    return (
+    <section className={classes.MealsLoading}>
+      Loading
+    </section>
+    )
+  }
+
+  if(httpError){
+    return (
+
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+
+    )
+  }
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
+
+
+/*
+
 const DUMMY_MEALS = [
   {
     id: 'm1',
@@ -30,63 +119,4 @@ const DUMMY_MEALS = [
   },
 ];
 
-const AvailableMeals = () => {
-
-  const [meals, setMeals] =useState([]);
-  const [isLoading , setIsLoading] =useState(true);
-
-  useEffect( () => {
-
-    const fetchMeals = async() => {
-
-      const response = await fetch('https://reactcg-http-default-rtdb.firebaseio.com/Meals.json')
-      const responseData = await response.json();
-
-      const loadedMeals = [];
-
-      for(const key in responseData){
-        loadedMeals.push({
-          id:key,
-          name: responseData[key].name,
-          description: responseData[key].description,
-          price: responseData[key].price,
-        })
-      }
-
-      setMeals(loadedMeals);
-      setIsLoading(false)
-
-    }
-    
-    fetchMeals();
-
-  },[])
-
-
-  const mealsList = meals.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
-
-  if(isLoading){
-    return (
-    <section className={classes.MealsLoading}>
-      Loading
-    </section>
-    )
-  }
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
-};
-
-export default AvailableMeals;
+*/
