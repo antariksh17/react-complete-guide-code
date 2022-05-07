2af4d3e7afcd3ca2927b7829ac9d9d9fa362f9c5
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-07 22:16:41 +0530

    18.15-18.19->redux-toolkit-migration

--- code/01-starting-project/src/components/Counter.js
+++ code/01-starting-project/src/components/Counter.js
@@ -3,6 +3,7 @@
 import classes from './Counter.module.css';
 
 import {useSelector, useDispatch} from 'react-redux'; 
+import { counterActions } from '../store/index';
 
 const Counter = () => {
 
@@ -12,21 +13,27 @@ const Counter = () => {
 
   const incrementHandler = () => {
 
-    dispatch({type: 'increment'})
+    //dispatch({type: 'increment'})
+    dispatch(counterActions.increment())
   }
 
   const decrementHandler = () => {
 
-    dispatch({type: 'decrement'})
+    //dispatch({type: 'decrement'})
+    dispatch(counterActions.decrement())
   }
 
   const increaseHandler =() => {
 
-    dispatch({type: 'increase', amount:5});
+    //dispatch({type: 'increase', amount:5});
+
+    dispatch(counterActions.increase(5))
   }
 
   const toggleCounterHandler = () => {
-    dispatch({type: 'toggle'})
+
+    //dispatch({type: 'toggle'})
+    dispatch(counterActions.toggleCounter())
   };
 
   return (
--- code/01-starting-project/src/store/index.js
+++ code/01-starting-project/src/store/index.js
@@ -1,6 +1,36 @@
-import {createStore} from 'redux';
+// import {createStore} from 'redux';
+import {createSlice, configureStore} from '@reduxjs/toolkit'
 
 const initialState = {counter: 0, showCounter: false}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state){
+            state.counter++;
+        },
+        decrement(state){
+            state.counter--;
+        },
+        increase(state, action){
+            state.counter=state.counter +action.payload
+        },
+        toggleCounter(state){
+            state.showCounter= !state.showCounter
+        }
+    }
+})
+
+export const counterActions = counterSlice.actions
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
+
+export default store;
+/*
+
 const reducerFunction = (state= initialState, action) => {
 
     if(action.type === 'increment'){
@@ -31,7 +61,4 @@ const reducerFunction = (state= initialState, action) => {
 }
 
 
-const store = createStore(reducerFunction);
-
-
-export default store;
+*/
