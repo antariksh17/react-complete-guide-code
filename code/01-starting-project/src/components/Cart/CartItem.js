5fb9ae51b2393558f6063e3cfa092503efc0a165
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-08 19:00:08 +0530

    19.3+4->refresher-completed

--- code/01-starting-project/src/components/Cart/CartItem.js
+++ code/01-starting-project/src/components/Cart/CartItem.js
@@ -1,7 +1,27 @@
 import classes from './CartItem.module.css';
 
+import { cartActions } from '../../store/cart-slice';
+import{useDispatch} from 'react-redux';
+
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price,id } = props.item;
+
+  const dispatch= useDispatch();
+
+  const removeitemsHandler= () => {
+    dispatch(cartActions.removeItemFromCart(id));
+
+  }
+
+  const addItemsHandler = () => {
+
+    dispatch(cartActions.addItemToCart({
+      id,
+      title,
+      price
+    }))
+
+  }
 
   return (
     <li className={classes.item}>
@@ -17,8 +37,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeitemsHandler} > - </button>
+          <button onClick={addItemsHandler} >+</button>
         </div>
       </div>
     </li>
