64778e2ce127c53b86b334d5f05311fd97702228
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-10 19:43:16 +0530

    19.11->using-action-creator

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -4,7 +4,8 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/ui-slice';
+// import { uiActions } from './store/ui-slice';
+import {sendCartData} from './store/cart-slice'
 
 let isInitial= true;
 
@@ -23,48 +24,14 @@ function App() {
 
   useEffect( () => {
   
-    const sendCartData = async () => {
-
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!'
-      }))
-
-      const response = await fetch('https://reactcg-http-default-rtdb.firebaseio.com/cart.json', { 
-        method: 'PUT', 
-        body: JSON.stringify(cart)
-      })
-
-      if(!response.ok){
-
-        throw new Error('Sending cart data failed')
-
-      }
-
-      dispatch(uiActions.showNotification({
-        status: 'Success',
-        title: 'Success!',
-        message: 'Sent cart data successfully!'
-      }))
-  
-      //const responseData= await response.json()
-
-    }
 
     if(isInitial){
       isInitial=false;
       return;
     }
     
-    sendCartData().catch((error) => {
+    dispatch(sendCartData(cart))
 
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'error',
-        message: 'Sending cart data failed!'
-      }))
-    })
 
   },[cart, dispatch]);
 
