57f74747b1e62e3b6acd983bf6b6a01496e98593
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-11 19:31:21 +0530

    19.9->configuring-routes-using-switch-n-exact

--- code/01-starting-project/src/pages/Products.js
+++ code/01-starting-project/src/pages/Products.js
@@ -1,13 +1,13 @@
-
+import {Link}  from 'react-router-dom';
 
 const Products = () => {
     return ( 
     <section>
         <h1>Product header</h1>
         <ul>
-            <li>Book</li>
-            <li>carpet</li>
-            <li>online course</li>
+            <li><Link to="/products/p1" >Book</Link></li>
+            <li><Link to="/products/p1" >carpet</Link></li>
+            <li><Link to="/products/p1" >online course</Link></li>
         </ul>
     
     </section>
