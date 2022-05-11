988a8f299c5a67207b1ecc77804c660fabea6a96
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-11 19:49:15 +0530

    20.10->nested-routes

--- code/01-starting-project/src/pages/Welcome.js
+++ code/01-starting-project/src/pages/Welcome.js
@@ -1,6 +1,14 @@
+import {Route}  from 'react-router-dom'
 
 const Welcome = () => {
- return <h1>Welcome page</h1>
+ return (
+        <section>
+            <h1>Welcome page</h1>
+            <Route path="/welcome/new-user">
+                <p>Wekcome, New User!</p>
+            </Route>
+        </section>
+    )
 };
 
 export default Welcome;
