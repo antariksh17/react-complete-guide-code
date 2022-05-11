39820b5adf8ad1b59d6d215b559a1c1edde01975
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-12 03:50:20 +0530

    20.14->nesting-practice

--- code/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ code/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,6 +1,6 @@
 import {Fragment} from 'react';
-import { useParams } from "react-router-dom";
-
+import { Route, useParams } from "react-router-dom";
+import Comments from '../components/comments/Comments'
 const QuoteDetail = () => {
 
     const params = useParams();
@@ -11,6 +11,10 @@ const QuoteDetail = () => {
             
             <h1> Page</h1>
             <p>{params.quoteId}</p>
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments />
+
+            </Route>
 
         </Fragment>
        
