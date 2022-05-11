bb0f866d182f61e0f06e8c0305ceea4245f2a9f5
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-12 03:46:19 +0530

    20.13->redirecting-practice

--- code/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ code/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,6 +1,20 @@
+import {Fragment} from 'react';
+import { useParams } from "react-router-dom";
 
 const QuoteDetail = () => {
-    return <h1> Page</h1>
+
+    const params = useParams();
+    console.log(params.quoteId)
+
+    return (
+        <Fragment>
+            
+            <h1> Page</h1>
+            <p>{params.quoteId}</p>
+
+        </Fragment>
+       
+    )
 }; 
 
 export default QuoteDetail;
