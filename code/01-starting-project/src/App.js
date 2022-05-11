7586b94110a6bf0dfa338eb8afa6b4814112ed8b
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-11 19:08:51 +0530

    20.5-20.8->Links-n-dynamic-routes-extracting-param

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,4 +1,6 @@
 import {Route} from 'react-router-dom';
+import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
 import Products from './pages/Products';
 
 import Welcome from './pages/Welcome'
@@ -6,12 +8,21 @@ import Welcome from './pages/Welcome'
 function App() {
   return (
     <div>
-        <Route path='/welcome' >
-          <Welcome />
-        </Route>
-        <Route path='/products' >
-          <Products />
-        </Route>
+
+        <div>
+          <MainHeader />
+        </div>
+        <main>
+          <Route path='/welcome' >
+            <Welcome />
+          </Route>
+          <Route path='/products' >
+            <Products />
+          </Route>
+          <Route path="/product-detail/:productId">
+              <ProductDetail />
+          </Route>
+        </main>
 
     </div>
   );
--- code/01-starting-project/src/pages/Products.js
+++ code/01-starting-project/src/pages/Products.js
@@ -1,7 +1,18 @@
 
 
 const Products = () => {
-    return <h1>Product header</h1>
+    return ( 
+    <section>
+        <h1>Product header</h1>
+        <ul>
+            <li>Book</li>
+            <li>carpet</li>
+            <li>online course</li>
+        </ul>
+    
+    </section>
+    )
+
 }
 
 export default Products;
