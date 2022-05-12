8859605db2ab1b76ad3e8c10438ebfe66d712deb
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-12 16:24:06 +0530

    20.16-17->dummy-data+-o/p-data-on-details-page

--- code/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ code/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,24 +1,31 @@
-import {Fragment} from 'react';
-import { Route, useParams } from "react-router-dom";
-import Comments from '../components/comments/Comments'
+import { Fragment } from 'react';
+import { useParams, Route } from 'react-router-dom';
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
 const QuoteDetail = () => {
+  const params = useParams();
 
-    const params = useParams();
-    console.log(params.quoteId)
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-    return (
-        <Fragment>
-            
-            <h1> Page</h1>
-            <p>{params.quoteId}</p>
-            <Route path={`/quotes/${params.quoteId}/comments`}>
-                <Comments />
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
 
-            </Route>
-
-        </Fragment>
-       
-    )
-}; 
+  return (
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
 
 export default QuoteDetail;
